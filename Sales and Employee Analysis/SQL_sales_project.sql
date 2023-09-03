496f57b95c39ff38cdbdf2d8cf195f918218b3b5
Cem Tener <112777266+CemTener1@users.noreply.github.com>
2023-09-03 12:25:54 -0700

    Add files via upload

---
+++ Sales and Employee Analysis/SQL_sales_project.sql
@@ -0,0 +1,100 @@
+USE sql_project;
+
+-- add random dates 
+
+ALTER TABLE sales
+ADD sale_date DATE;
+
+UPDATE sales
+SET sale_date = DATE_ADD('2021-01-01', INTERVAL FLOOR(RAND() * 365*3) DAY);
+
+-- add price to products with $0 price
+UPDATE products
+SET Price = CASE
+WHEN Price = 0 THEN ROUND(RAND() * 100) + 1 
+ELSE Price
+END;
+
+-- Calculate customer lifetime
+CREATE VIEW customer_lifetime AS
+SELECT CustomerID,
+		CONCAT(
+		TIMESTAMPDIFF(YEAR,MIN(sale_date), MAX(sale_date)), ' years and ',
+        TIMESTAMPDIFF(MONTH,MIN(sale_date), MAX(sale_date)) - 
+        TIMESTAMPDIFF(YEAR,MIN(sale_date), MAX(sale_date)) * 12, ' months '
+        )
+        AS customer_lifetime
+FROM sales 
+GROUP BY CustomerID;
+
+-- arcived and active orders 
+
+SELECT *,
+		CASE
+        WHEN YEAR(sale_date) = YEAR(NOW()) THEN 'Active'
+        WHEN YEAR(sale_date) = YEAR(NOW()) - 1 THEN 'Last Year'
+        ELSE 'Archived'
+        END as date_status
+FROM sales;
+
+
+-- Find the revenue generated from each product
+
+SELECT ProductID,
+		Name,
+        Price,
+        SUM(Quantity) AS total_sales,
+        ROUND(SUM(Quantity) * Price, 2 ) AS Revenue
+FROM sales s
+JOIN products p USING(productID)
+GROUP BY ProductID , Price, Name, Price
+ORDER BY Revenue DESC
+LIMIT 10;
+
+-- employees that bring in the highest revenue
+
+CREATE VIEW best_employees AS 
+SELECT SalesPersonID,
+        CONCAT(FirstName,' ', MiddleInitial,' ',LastName) AS SalesmanName,
+        ROUND(SUM(Quantity) * Price, 2 ) AS revenue_brought,
+        SUM(Quantity) AS units_sold
+FROM sales s 
+JOIN employees e ON s.SalesPersonID = e.employeeID
+JOIN products USING (productID)
+GROUP BY SalesPersonID, FirstName, MiddleInitial, LastName, Price
+ORDER BY revenue_brought DESC;
+
+-- Point system for employees (.2*units sold + .8 revenue)/10000000 and categorize
+
+SELECT *,
+       CEILING((revenue_brought * 0.6 + 0.35 * units_sold) / 10000) AS points,
+       CASE 
+			WHEN (SELECT points ) > 2000 THEN 'Elite'
+            WHEN (SELECT points) > 1000 THEN 'Top'
+            WHEN (SELECT points) > 700 THEN 'High'
+            WHEN (SELECT points )> 400 THEN 'Consistent' 
+            ELSE 'Emerging'
+            END AS Ranking
+	
+FROM best_employees;
+
+-- Products with higher price than the average using correlated subqueries
+
+SELECT *
+FROM products p
+WHERE Price > 
+				(SELECT AVG(Price)
+                FROM products 
+                WHERE Price = p.Price);
+                
+
+ 
+
+
+
+
+
+
+
+
+
---
+++ Seattle Weather Prediction/Seattle Weather-Decision Tree.R
@@ -0,0 +1,45 @@
+# Load required libraries
+library(tidyverse)
+library(dplyr)
+library(caret)
+library(ISLR)
+library(rpart)
+library(rpart.plot)
+
+#quick look at the data
+head(mydata)
+#select the necessary columns
+mydata<- mydata %>%
+  select(precipitation, temp_max, temp_min, wind, weather)
+head(mydata)
+
+
+set.seed(2000)
+train.index=sample(1:dim(mydata)[1],dim(mydata)[1]*0.8)
+train.df=mydata[train.index,]
+valid.df=mydata[-train.index,]
+
+default.ct=rpart(weather~.,data=train.df,method="class",control=rpart.control(maxdepth=4))
+rpart.plot(default.ct,type=1,extra=2,under=TRUE,split.font=1,varlen=-5)
+
+#Predict using first model
+default.ct=as.factor(predict(default.ct,valid.df,type="class"))
+valid.df.Loan=as.factor(valid.df$weather)
+confusionMatrix(default.ct,valid.df.Loan)
+
+# deeper tree
+deeper.ct=rpart(weather~.,data=train.df,method="class",cp=0,minsplit=1)
+length(deeper.ct$frame$var[deeper.ct$frame$var=="<leaf>"])
+
+prp(deeper.ct,type=1,extra=1,under=TRUE,split.font=1,varlen=-5,box.col=ifelse(deeper.ct$frame$var=="<leaf>",'blue','red'))
+
+#Prediction using test data
+deeper.ct.pred.valid=as.factor(predict(deeper.ct,valid.df,type="class"))
+confusionMatrix(deeper.ct.pred.valid,valid.df.Loan)
+
+#From this example we can understand that making a model more complex does not make it better.
+
+
+
+
+
